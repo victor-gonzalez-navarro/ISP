211b28296c1c84e103ccb6acdfc44df9f3d4752c
Alejandro Ariza <alejandro.ariza14@gmail.com>
2019-11-07 18:15:47 +0100

    Add LSTM Global, ARIMA and Integration

---
+++ src/lstm_global.py
@@ -0,0 +1,68 @@
+import numpy as np
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, LSTM
+from keras.callbacks import ModelCheckpoint
+from utils import color_gen
+from sklearn import linear_model
+
+
+class LSTM_Global:
+
+    def __init__(self, config, name=None, model_path=None):
+        if model_path is None:
+            self.name = name
+            self.model = Sequential(name=name)
+        else:
+            self.model = load_model(model_path)
+            self.name = self.model.name
+        self.config = config
+
+    def buildLayers(self):
+        self.model.add(LSTM(self.config['layers_lstm_global']['neurons'][0],
+                            input_shape=(self.config['layers_lstm_global']['timesteps'], 2),
+                            return_sequences=True))
+        self.model.add(Dropout(self.config["layers_lstm_global"]["droprate"][0]))
+        self.model.add(LSTM(self.config['layers_lstm_global']['neurons'][1],
+                            input_shape=(None, None), #(self.config['layers_lstm_global']['timesteps'], None),
+                            return_sequences=True))
+        self.model.add(LSTM(self.config['layers_lstm_global']['neurons'][2],
+                            input_shape=(None, None), #(self.config['layers_lstm_global']['timesteps'], None),
+                            return_sequences=False))
+        self.model.add(Dropout(self.config["layers_lstm_global"]["droprate"][1]))
+        self.model.add(Dense(self.config['layers_lstm_global']['timesteps'],
+                             activation=self.config['layers_lstm_global']['activationDense']))
+
+        self.model.compile(loss=self.config['layers_lstm_global']['loss'],
+                           optimizer=self.config['layers_lstm_global']['optimizer'])
+
+    def fit(self, generator, n_seqs):
+        callbacks = [
+            ModelCheckpoint(filepath='../models/'+self.name, monitor='loss', save_best_only=True)
+        ]
+
+        steps_per_epoch = np.ceil(n_seqs/self.config['batch_size'])
+        self.model.fit_generator(generator=generator, steps_per_epoch=steps_per_epoch,
+                                 epochs=self.config['num_epochs'], callbacks=callbacks, workers=1)
+
+    def predict(self, tst_data, plot=None):
+        y_pred = np.zeros((len(tst_data), len(tst_data[0][1])))
+        y_pred_reg = np.copy(y_pred)
+
+        color = color_gen()
+        for seq_id in range(len(tst_data)):
+            y_pred[seq_id, :] = self.model.predict(np.array([tst_data[seq_id][0]]))
+
+            # Create linear regression object
+            regr = linear_model.LinearRegression()
+            regr.fit(np.array(range(y_pred.shape[1])).reshape(y_pred.shape[1], 1), y_pred[seq_id, :])
+            y_pred_reg[seq_id, :] = regr.predict(np.array(range(y_pred.shape[1])).reshape(y_pred.shape[1], 1))
+
+            if plot is not None:
+                start = seq_id * y_pred.shape[1]
+                end = start + y_pred.shape[1]
+                plot.plot(list(range(start, end)), y_pred[seq_id, :], next(color) + '^-', markersize=3,
+                          linewidth=0.5)
+                plot.plot(list(range(start, end)), y_pred_reg[seq_id, :], next(color) + '^-', markersize=3,
+                          linewidth=0.5)
+
+        return y_pred_reg
--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import json
+import warnings
+import pickle
 import matplotlib.pyplot as plt
-from utils import preprocess
+
+from lstm_global import LSTM_Global
+from model2 import Model2
+from utils import preprocess, preprocess_all, preprocess_all_extra, color_gen, predict_arima, create_dataset_lstm_global
 from utils import read_data
 from utils import create_gen
 from model import Model
 import datetime
+import numpy as np
+from statsmodels.tsa.arima_model import ARIMA
+from tqdm import tqdm
 
 
 def main_new_model():
@@ -17,6 +25,7 @@ def main_new_model():
     gen = create_gen(seqs_train, config['batch_size'])
 
     fig = plt.figure()
+    fig.suptitle('Model 1')
     sp = fig.add_subplot(1, 1, 1)
     sp.plot(list(range(len(seqs_test))), [y for x, y in seqs_test], linewidth=0.5)
 
@@ -28,6 +37,30 @@ def main_new_model():
 
     plt.show()
 
+
+def main_new_model_all():
+    with open('../config/params2.json', 'r') as read_file:
+        config = json.load(read_file)
+
+    data = read_data('../data/sp500.csv')
+    seqs_train, seqs_test = preprocess_all(data, config)
+
+    gen = create_gen(seqs_train, config['batch_size'])
+
+    fig = plt.figure()
+    fig.suptitle('Model 2')
+    sp = fig.add_subplot(1, 1, 1)
+    sp.plot(list(range(len(seqs_test))), [y[0] for x, y in seqs_test], linewidth=0.5)
+
+    m = Model2(config, name='Model2_{date:%Y-%m-%d_%H_%M_%S}'.format(date=datetime.datetime.now()))
+    m.buildLayers()
+    m.fit(gen, len(seqs_train))
+
+    m.predict(seqs_test, next_k_items=config['next_k_items'], plot=sp)
+
+    plt.show()
+
+
 def main_best_model():
     with open('../config/params.json', 'r') as read_file:
         config = json.load(read_file)
@@ -35,19 +68,8 @@ def main_best_model():
     data = read_data('../data/sp500.csv')
     seqs_train, seqs_test = preprocess(data, config)
 
-    gen = create_gen(seqs_train, config['batch_size'])
-
-    yii = next(gen)
-    a = yii[0][1, :, :]
-    a2 = yii[0][6, :, :]
-    yii2 = next(gen)
-    b = yii2[0][1, :, :]
-    b2 = yii2[0][6, :, :]
-    yii3 = next(gen)
-    c = yii3[0][1, :, :]
-    c2 = yii3[0][6, :, :]
-
     fig = plt.figure()
+    fig.suptitle('Model 1')
     sp = fig.add_subplot(1, 1, 1)
     sp.plot(list(range(len(seqs_test))), [y for x, y in seqs_test], linewidth=0.5)
 
@@ -59,4 +81,161 @@ def main_best_model():
     print('Finished')
 
 
-main_new_model()
+def main_best_model_all():
+    with open('../config/params2.json', 'r') as read_file:
+        config = json.load(read_file)
+
+    data = read_data('../data/sp500.csv')
+    seqs_train, seqs_test = preprocess_all(data, config)
+
+    fig = plt.figure()
+    fig.suptitle('Model 2')
+    sp = fig.add_subplot(1, 1, 1)
+    sp.plot(list(range(len(seqs_test))), [y[0] for x, y in seqs_test], linewidth=0.5)
+
+    m = Model2(config, model_path='../models/Model2_2019-10-17_14_35_54')
+    m.predict(seqs_test, next_k_items=config['next_k_items'], plot=sp)
+
+    plt.show()
+
+    print('Finished')
+
+
+def main_fourier():
+    with open('../config/params2.json', 'r') as read_file:
+        config = json.load(read_file)
+
+    data = read_data('../data/sp500.csv')
+
+    seqs_train, seqs_test = preprocess_all_extra(data, config)
+
+    gen = create_gen(seqs_train, config['batch_size'])
+
+    fig = plt.figure()
+    fig.suptitle('Model 2')
+    sp = fig.add_subplot(1, 1, 1)
+    sp.plot(list(range(len(seqs_test))), [y[0] for x, y in seqs_test], linewidth=0.5)
+
+    m = Model2(config, name='Model2_{date:%Y-%m-%d_%H_%M_%S}'.format(date=datetime.datetime.now()))
+    output_feats = seqs_train[0][0].shape[1]
+    m.buildLayers(output_feats)
+    m.fit(gen, len(seqs_train))
+
+    m.predict(seqs_test, next_k_items=config['next_k_items'], plot=sp)
+
+    plt.show()
+
+    print('Finished')
+
+
+def main_best_fourier():
+    pass
+
+
+def main_ARIMA():
+    warnings.filterwarnings("ignore")
+
+    with open('../config/params2.json', 'r') as read_file:
+        config = json.load(read_file)
+
+    data = read_data('../data/sp500.csv')
+
+    seqs_train, seqs_test = preprocess_all_extra(data, config)
+
+    gen = create_gen(seqs_train, config['batch_size'])
+
+    fig = plt.figure()
+    fig.suptitle('Model 2')
+    sp = fig.add_subplot(1, 1, 1)
+    sp.plot(list(range(len(seqs_test))), [y[0] for x, y in seqs_test], linewidth=2)
+
+    m = Model2(config, name='Model2_{date:%Y-%m-%d_%H_%M_%S}'.format(date=datetime.datetime.now()))
+    output_feats = seqs_train[0][0].shape[1]
+    m.buildLayers(output_feats)
+    print('Fitting LSTM model...')
+    m.fit(gen, len(seqs_train))
+    print('Obtaining Train Predictions by LSTM model...')
+    lstm_preds_trn = m.predict(seqs_train, next_k_items=config['next_k_items'])
+
+    print('Fitting ARIMA model...')
+    arima_preds_trn = predict_arima(seqs_train, config, next_k_items=config['next_k_items'])
+
+    f = open('./arima_preds.pckl', 'wb')
+    pickle.dump(arima_preds_trn, f)
+    f.close()
+
+    f2 = open('./lstm_preds.pckl', 'wb')
+    pickle.dump(lstm_preds_trn, f2)
+    f2.close()
+
+def main_other():
+    warnings.filterwarnings("ignore")
+
+    with open('../config/params2.json', 'r') as read_file:
+        config = json.load(read_file)
+
+    data = read_data('../data/sp500.csv')
+
+    seqs_train, seqs_test = preprocess_all_extra(data, config)
+
+    m = Model2(config, model_path='../models/Model2_2019-11-07_16_31_01')
+
+    f = open('./arima_preds.pckl', 'rb')
+    arima_preds_trn = pickle.load(f)
+    f.close()
+
+    f2 = open('./lstm_preds.pckl', 'rb')
+    lstm_preds_trn = pickle.load(f2)
+    f2.close()
+
+    fig = plt.figure()
+    fig.suptitle('Model 2')
+    sp = fig.add_subplot(1, 1, 1)
+    sp.plot(list(range(len(seqs_test))), [y[0] for x, y in seqs_test], linewidth=2)
+
+    trn_samples = round(config['train_test_split'] * data.shape[0])
+    seqs_train2 = create_dataset_lstm_global(lstm_preds_trn,
+                                             arima_preds_trn,
+                                             data[config['features']].values[:(trn_samples + config['next_k_items']),
+                                                  config['features'].index('Close')])
+
+    gen2 = create_gen(seqs_train2, config['batch_size'])
+
+    lstm_global = LSTM_Global(config, name='LSTM_Global_{date:%Y-%m-%d_%H_%M_%S}'.format(date=datetime.datetime.now()))
+    lstm_global.buildLayers()
+    print('Fitting LSTM Global model...')
+    lstm_global.fit(gen2, len(seqs_train2))
+
+    print('Starting ARIMA Predictions...')
+    arima_preds_tst = predict_arima(seqs_test, config, next_k_items=config['next_k_items'], plot=sp)
+    print('ARIMA Predictions Finished!')
+
+    print('Starting LSTM Predictions...')
+    lstm_preds_tst = m.predict(seqs_test, next_k_items=config['next_k_items'], plot=sp)
+    print('LSTM Predictions Finished!')
+
+    seqs_test2 = create_dataset_lstm_global(lstm_preds_tst,
+                                            arima_preds_tst,
+                                            data[config['features']].values[(trn_samples + config['next_k_items']):,
+                                            config['features'].index('Close')])
+
+    lstm_global_preds = lstm_global.predict(seqs_test2, plot=sp)
+
+    plt.show()
+
+    print('Finished')
+
+
+
+
+main_other()
+# x = np.sin(np.linspace(0, 3 * 2 * np.pi, 3 * 360)) + np.random.normal(0, 0.011, 3 * 360)
+#
+# plt.plot(x)
+#
+# arima = ARIMA(x, order=(50,1,0))
+#
+# arima_fit = arima.fit(disp=0)
+#
+# plt.plot(list(range(3 * 360 + 1, 3 * 360 + 200 + 1)), arima_fit.forecast(200)[0], 'r')
+# plt.show()
